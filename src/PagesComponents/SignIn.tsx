75cc4c5fcf4c218b207ab80b24b1cf89c30b8595
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-03-31 13:33:57 -0300

    feat: add the login function and alerts in case of errors and invalid information

--- src/PagesComponents/SignIn.tsx
+++ src/PagesComponents/SignIn.tsx
@@ -1,12 +1,22 @@
 'use client';
 
-import { login, ping } from '@/requests/script';
-import { Button, Card, CardBody, Flex, Input } from '@chakra-ui/react';
+import { login } from '@/requests/script';
+import { emailTester, passwordTester } from '@/utils/regexTester';
+import {
+	Button,
+	Card,
+	CardBody,
+	Flex,
+	Input,
+	useToast,
+} from '@chakra-ui/react';
 import { useState } from 'react';
 
 const SignIn = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+
+	const toast = useToast();
 
 	const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) =>
 		setEmail(e.target.value);
@@ -15,10 +25,62 @@ const SignIn = () => {
 		setPassword(e.target.value);
 
 	const handleSignIn = async () => {
-		if(email === '' || password === '') return alert('Please, fill all the camps');
+		if (email === '' || password === '')
+			return alert('Please, fill all the camps');
+		let errorToast;
+		let infoToast;
+		if (!emailTester(email)) {
+			return (errorToast = toast({
+				title: 'Invalid Email',
+				description: 'Please, write a valid email',
+				status: 'warning',
+				duration: 5000,
+				isClosable: true,
+			}));
+		}
+		if (!passwordTester(password)) {
+			return (errorToast = toast({
+				title: 'Invalid password',
+				description:
+					'The password must have: 8 characters, one lowercase letter, one uppercase letter and one digit',
+				status: 'warning',
+				duration: 5000,
+				isClosable: true,
+			}));
+		}
 		console.log('SignIn');
-		const data = await login({email, password});
-		console.log(data?.status);
+		try {
+			const data = await login({ email, password });
+			if (data.status === 'ok') {
+				return toast({
+					title: 'Login success',
+					description: 'OK!',
+					status: 'success',
+					duration: 3000,
+					isClosable: true,
+				});
+			}
+			toast({
+				title: 'User not found',
+				description: 'please, check the email and password',
+				status: 'error',
+				duration: 3000,
+				isClosable: true,
+			});
+			console.log(data);
+		} catch (error) {
+			console.log('fudeu');
+			if (errorToast) {
+				toast.close(errorToast);
+			}
+			toast({
+				title: 'Error',
+				description: 'An error have ocurred!',
+				status: 'error',
+				duration: 5000,
+				isClosable: true,
+			});
+		}
 	};
 
 	return (
@@ -32,14 +94,14 @@ const SignIn = () => {
 					<Flex flexDirection='column' gap='10' padding='5'>
 						<Input
 							value={email}
-							placeholder='Email'
+							placeholder='example@example.com'
 							type='email'
 							required
 							onChange={handleEmailChange}
 						/>
 						<Input
 							value={password}
-							placeholder='Password'
+							placeholder='Example1'
 							required
 							onChange={handlePasswordChange}
 							type='password'
