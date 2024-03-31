a18e80f6c5cd6909d536d17ff696473496671485
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-03-31 13:33:23 -0300

    fix: add the error return in case of errors

--- src/requests/script.ts
+++ src/requests/script.ts
@@ -1,7 +1,7 @@
 import { loginReturn } from '@/types/loginReturn';
 
 export const ping = async () => {
-  try {
+	try {
 		const response = await fetch('http://localhost:80/ping/');
 		const data = await response.json();
 		return data;
@@ -9,24 +9,30 @@ export const ping = async () => {
 		console.log(`Error: `, error);
 		return null;
 	}
-}
+};
 
-export const login = async ({email, password}: {email: string, password: string}) => {
-  const formData = new URLSearchParams();
-  formData.append('email', email);
-  formData.append('password', password);
-  try {
-    const response = await fetch('http://localhost:80/user/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: formData,
-    });
-    const data = await response.json();
-    return data as loginReturn;
-  } catch(error) {
-    console.log(`Error: `, error);
-    return null;
-  }
-}
+export const login = async ({
+	email,
+	password,
+}: {
+	email: string;
+	password: string;
+}) => {
+	const formData = new URLSearchParams();
+	formData.append('email', email);
+	formData.append('password', password);
+	try {
+		const response = await fetch('http://localhost:80/user/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: formData,
+		});
+		const data = await response.json();
+		return data as loginReturn;
+	} catch (error) {
+		console.log(`Error: `, error);
+    throw new Error('Ocorreu um erro durante a solicitação');
+	}
+};
